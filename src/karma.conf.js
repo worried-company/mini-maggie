cf5a0598fff23a2cea4004b8ccb3f6949bfeb740
Hugo Soares <24919339+worriedhugo@users.noreply.github.com>
2019-02-04 17:49:20 +0000

    PS00 - Project Setup
    Karma changes

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
