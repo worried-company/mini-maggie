d1128f3c2eaca642289f51cd1558a9075adde323
Hugo Soares <24919339+worriedhugo@users.noreply.github.com>
2019-02-14 11:37:01 +0000

    PS00 - Project Setup
    Karma fix

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -35,3 +38,5 @@ module.exports = function (config) {
     singleRun: false
   });
 };
+
+
